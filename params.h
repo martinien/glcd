e9222b1257077fd53a00eb07c3339431bf885cbc
Martin Sorel <sorel.martin@gmail.com>
2016-02-08 16:40:59 +0100

    Two bars version

---
+++ params.h
@@ -0,0 +1,22 @@
+/* 
+ * File:   params.h
+ * Author: martinien
+ *
+ * Created on 8 février 2016, 16:30
+ */
+
+#ifndef PARAMS_H
+#define	PARAMS_H
+
+#define TWO_BARS
+#ifdef TWO_BARS
+#define BAR_WIDTH 40
+#define BAR_SPAN 48
+#else
+#define BAR_WIDTH 20
+#define BAR_SPAN 24
+#endif
+#define MENU_WIDTH 28
+
+#endif	/* PARAMS_H */
+
